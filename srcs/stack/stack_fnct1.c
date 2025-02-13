4cc99b9b8d45307af999baa8c5221375770e6917
Talha <mutlutalha02@gmail.com>
2025-02-13 17:42:05 +0100

    edidted the header file

--- srcs/stack/stack_fnct1.c
+++ srcs/stack/stack_fnct1.c
@@ -1,31 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   stack_fnct1.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tamutlu <tamutlu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/10 19:22:04 by tamutlu           #+#    #+#             */
-/*   Updated: 2025/02/13 17:40:21 by tamutlu          ###   ########.fr       */
+/*   Created: 2025/02/13 14:45:26 by tamutlu           #+#    #+#             */
+/*   Updated: 2025/02/13 14:45:27 by tamutlu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef STACK_H
-# define STACK_H
-
-# include "ft_printf.h"
-# include <limits.h>
-# include <stdbool.h>
-# include <stdlib.h>
-
-// struct represting a node in the linked list
-// typedef struct Node
-// {
-// 	int data;
-// 	struct Node* next;
-	
-	
-// }
-
-
-#endif
