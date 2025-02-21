f1fd4c1755081991c5ccbba7ed993a014be52967
Talha <mutlutalha02@gmail.com>
2025-02-21 14:17:49 +0100

    finsihed the rules

--- srcs/rules/ft_swap_ops.c
+++ srcs/rules/ft_swap_ops.c
@@ -6,7 +6,44 @@
 /*   By: tamutlu <tamutlu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 14:30:35 by tamutlu           #+#    #+#             */
-/*   Updated: 2025/02/19 14:30:36 by tamutlu          ###   ########.fr       */
+/*   Updated: 2025/02/20 17:43:52 by tamutlu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+void	swap_a(t_list **stackA)
+{
+	t_list	*first;
+	t_list	*second;
+
+	first = *stackA;
+	second = first->next;
+	if (!stackA || !*stackA || !(*stackA)->next)
+		return ;
+	first->next = second->next;
+	second->next = first;
+	*stackA = second;
+	write(1, "sa\n", 3);
+}
+
+void	swap_b(t_list **stackB)
+{
+	t_list	*first;
+	t_list	*second;
+
+	first = *stackB;
+	second = first->next;
+	if (!stackB || !*stackB || !(*stackB)->next)
+		return ;
+	first->next = second->next;
+	second->next = first;
+	*stackB = second;
+	write(1, "sb\n", 3);
+}
+
+void	swap_ab(t_list **stackA, t_list **stackB)
+{
+	swap_a(stackA);
+	swap_b(stackB);
+}
