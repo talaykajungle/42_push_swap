b62bafde2f1f1359674b9c130eec4ae900256369
Talha <mutlutalha02@gmail.com>
2025-02-22 17:25:13 +0100

    added sort.c file

--- srcs/sorts/sort.c
+++ srcs/sorts/sort.c
@@ -1,18 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   radix_sort.c                                       :+:      :+:    :+:   */
+/*   sort.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tamutlu <tamutlu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 17:24:26 by tamutlu           #+#    #+#             */
-/*   Updated: 2025/02/21 17:28:20 by tamutlu          ###   ########.fr       */
+/*   Updated: 2025/02/22 16:50:02 by tamutlu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	radix_sort(t_list *stackA)
+void	sort_3(t_list **stackA, t_list **stackB)
+{
+	t_list	*stackA;
+	
+	*stackA = 0;
+	if (*stackA == 0)
+	{
+		/* code */
+	}
+	
+}
+
+void	sort_5(t_list **stackA, t_list **stackB)
 {
 	
 }
+
